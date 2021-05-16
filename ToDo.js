44defd1af2c1736735d5f1f7d888b30afd8053c3
Alvaro <astaboada7@gmail.com>
2021-05-16 20:52:54 +0200

    complete feature first test

--- ToDo.js
+++ ToDo.js
@@ -1,7 +1,11 @@
 class ToDo {
   constructor(text) {
     this.text = text;
-    this.id = 1;
     this.complete = false;
+  }
+  
+
+  set ItemComplete(complete){
+    this.complete = complete;
   }
 }
--- test/test.js
+++ test/test.js
@@ -6,10 +6,21 @@ describe('test', () => {
 });
 
 describe("todo items", () => {
+  const todo = new ToDo("Buy a coffee.", 1);
   it("allows us to create a new todo", () => {
-    const todo = new ToDo("Buy a coffee.");
     chai.expect(todo).to.not.eql(undefined);
   })
+  it("todo items contains Buy a coffee.", () => {
+    chai.expect(todo.text).deep.eql("Buy a coffee.");
+  })
+  it("todo items is complete = false", () => {
+    chai.expect(todo.complete).eql(false);
+  })
+  it("todo items can be complete", () => {
+    todo.ItemComplete = true;
+    chai.expect(todo.complete).eql(true);
+  })
 })
+
 
 mocha.run();
