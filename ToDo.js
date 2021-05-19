2a1fda5893eb0a0bdeae839cb54182bbdc9bfafa
Alvaro <astaboada7@gmail.com>
2021-05-19 22:30:27 +0200

    html form added, array and push function

--- ToDo.js
+++ ToDo.js
@@ -9,3 +9,19 @@ class ToDo {
     this.complete = complete;
   }
 }
+
+let toDos = [];
+
+const toDoInput = document.getElementById("toDo");
+const addForm = document.getElementById("addForm");
+addForm.addEventListener("submit", addItem);
+
+function addItem(ev){
+  ev.preventDefault();
+
+  let inputValue = toDoInput.value; 
+  const item = new ToDo(inputValue);
+  toDos.push(item);
+  toDoInput.value = "";
+  console.log(toDos);
+}
--- test/test.js
+++ test/test.js
@@ -23,4 +23,3 @@ describe("todo items", () => {
 })
 
 
-mocha.run();
